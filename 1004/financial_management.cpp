cbdf1aeff8578790d4140d855e5bd4c79a8b588f
kantuni <henrikh.kantuni@gmail.com>
2017-08-05 01:35:04 +0400

    1004 - Accepted

---
+++ 1004/financial_management.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double total = 0, balance;
+  int months = 12;
+  
+  for (int i = 0; i < months; ++i) {
+    cin >> balance;
+    total += balance;
+  }
+  
+  cout << "$" << total / months << "\n";
+  return 0;
+}
