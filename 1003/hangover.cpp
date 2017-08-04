718e2a41dbede78153c226f376a6f93464812b4c
kantuni <henrikh.kantuni@gmail.com>
2017-08-05 02:12:40 +0400

    1003 - Accepted

---
+++ 1003/hangover.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double c, total;
+  int i;
+
+  while (true) {
+    cin >> c;
+
+    if (c == 0) {
+      break;
+    }
+
+    total = 0.5;
+    i = 2;
+    while (total < c) {
+      ++i;
+      total += 1.0 / i;
+    }
+
+    cout << i - 1 << " card(s)" << "\n";
+  }
+  
+  return 0;
+}
