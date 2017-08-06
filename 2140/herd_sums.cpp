10e2feb03a9847f030d29eadc6f8cb8519196faa
kantuni <henrikh.kantuni@gmail.com>
2017-08-06 18:34:07 +0400

    2140 - Wrong answer

--- 2140/herd_sums.cpp
+++ 2140/herd_sums.cpp
@@ -5,6 +5,18 @@ typedef unsigned long long ull;
 
 int main() {
   ull N;
-  cin >> N; 
+  cin >> N;
+  
+  int counter = 1;
+  for (int i = 2; i <= (N + 1) / 2; ++i) {
+    // solution to x + (x + 1) + (x + 2) ... = N
+    int numerator = 2 * N - i * (i - 1);
+    int denominator = 2 * i;
+    if (numerator != 0 && numerator % denominator == 0) {
+      ++counter;
+    }
+  }
+  
+  cout << counter << "\n";
   return 0;
 }
