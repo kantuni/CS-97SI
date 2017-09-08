c05c0a863e4d44fc25959b2a62569910f2547838
kantuni <henrikh.kantuni@gmail.com>
2017-09-08 17:37:34 -0400

    2234 - Accepted

---
+++ 2234/matches_game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+typedef unsigned long long ull;
+
+int main() {
+  string line;
+  
+  while (getline(cin, line)) {
+    if (line.size() == 0) {
+      continue;
+    }
+    
+    istringstream iss(line);
+    ull m;
+    
+    iss >> m;
+    
+    ull nimsum = 0;
+    for (int i = 0; i < m; ++i) {
+      ull temp;
+      iss >> temp;
+      nimsum ^= temp;
+    }
+    
+    if (nimsum == 0) {
+      cout << "No" << "\n";
+    } else {
+      cout << "Yes" << "\n";
+    }
+  }
+  
+  return 0;
+}
