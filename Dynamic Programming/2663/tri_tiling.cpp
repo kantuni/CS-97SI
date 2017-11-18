57d07b965b175568ff770486202051e82582dfce
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-11-17 21:39:51 -0500

    Update tri_tiling.cpp

--- Dynamic Programming/2663/tri_tiling.cpp
+++ Dynamic Programming/2663/tri_tiling.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 using namespace std;
 
-typedef vector<int> vi;
-
 int main() {
   while (true) {
     int n;
@@ -13,21 +11,19 @@ int main() {
       break;
     }
     
-    vi D(n + 1);
+    vector<int> D(n + 1);
     D[0] = 1;
     D[1] = 0;
     
-    vi A(n + 1);
+    vector<int> A(n + 1);
     A[0] = 0;
     A[1] = 1;
     
-    for (int i = 2; i < n + 1; ++i) {
+    for (int i = 2; i < n + 1; i++) {
       A[i] = D[i - 1] + A[i - 2];
       D[i] = D[i - 2] + 2 * A[i - 1];
     }
-    
     cout << D[n] << "\n";
   }
-  
   return 0;
 }
