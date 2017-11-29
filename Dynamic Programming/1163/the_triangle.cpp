ffbeeab7b130902de408759825ba913b4846e51f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-11-29 11:58:19 -0500

    Update the_triangle.cpp

--- Dynamic Programming/1163/the_triangle.cpp
+++ Dynamic Programming/1163/the_triangle.cpp
@@ -9,22 +9,19 @@ typedef vector<vi> vvi;
 int main() {
   int n;
   cin >> n;
-  
   vvi t(n);
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; i++) {
     vi r(i + 1);
-    for (int j = 0; j < i + 1; ++j) {
+    for (int j = 0; j < i + 1; j++) {
       cin >> r[j];
     }
     t[i] = r;
   }
-  
-  for (int i = t.size() - 2; i >= 0; --i) {
-    for (int j = 0; j < t[i].size(); ++j) {
+  for (int i = t.size() - 2; i >= 0; i--) {
+    for (int j = 0; j < t[i].size(); j++) {
       t[i][j] += max(t[i + 1][j], t[i + 1][j + 1]);
     }
   }
-  
   cout << t[0][0] << "\n";
   return 0;
 }
