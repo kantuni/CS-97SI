990959e77d11ad74f10d27e0610eedcdc1764468
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-11-28 19:51:39 -0500

    Update financial_management.cpp

--- Coding Exercise/1004/financial_management.cpp
+++ Coding Exercise/1004/financial_management.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
   double total = 0, balance;
   int months = 12;
-  
-  for (int i = 0; i < months; ++i) {
+  for (int i = 0; i < months; i++) {
     cin >> balance;
     total += balance;
   }
-  
   cout << "$" << total / months << "\n";
   return 0;
 }
