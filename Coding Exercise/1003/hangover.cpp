f01d4609fd6f888a6059a1a688a633564f040dfd
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-05-24 08:19:58 -0400

    Update hangover.cpp

--- Coding Exercise/1003/hangover.cpp
+++ Coding Exercise/1003/hangover.cpp
@@ -2,25 +2,19 @@
 using namespace std;
 
 int main() {
-  double c, total;
-  int i;
-
   while (true) {
+    double c;
     cin >> c;
-
     if (c == 0) {
       break;
     }
-
-    total = 0.5;
-    i = 2;
+    double total = 0.5;
+    int i = 2;
     while (total < c) {
       i++;
       total += 1.0 / i;
     }
-
     cout << i - 1 << " card(s)" << "\n";
   }
-  
   return 0;
 }
