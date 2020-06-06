b8bfab60c334c7abac9e4527d7f0e2110c1410c6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-06 15:35:35 +0400

    Update adding_reversed_numbers.cpp

--- Coding Exercise/1504/adding_reversed_numbers.cpp
+++ Coding Exercise/1504/adding_reversed_numbers.cpp
@@ -16,25 +16,22 @@ ll reverse(ll x) {
 ll rsum(ll x, ll y) {
   ll rx = reverse(x);
   ll ry = reverse(y);
-  
   ll s = rx + ry;
   ll rs = reverse(s);
-  
   // remove trailing zeroes
   while (rs % 10 == 0) {
     rs /= 10;
   }
-  
   return rs;
 }
 
 int main() {
-  lll N;
-  cin >> N;
-  for (ll i = 0; i < N; i++) {
+  ll n;
+  cin >> n;
+  for (ll i = 0; i < n; i++) {
     ll x, y;
     cin >> x >> y;
-    cout << rsum(x, y) << "\n";
+    cout << rsum(x, y) << endl;
   }
   return 0;
 }
