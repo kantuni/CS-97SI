157f8b1a40e20e90771963fc74a295da82573496
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-07 20:44:49 +0400

    Update manhattan_2025.cpp

--- Coding Exercise/1806/manhattan_2025.cpp
+++ Coding Exercise/1806/manhattan_2025.cpp
@@ -9,12 +9,10 @@ typedef vector<vvc> vvvc;
 void draw(vvc &grid, int current) {
   int petrol = grid.size();
   int center = petrol / 2;
-  
   if (current == 1) {
     grid[center][center] = '0' + center;
     return;
   }
-  
   for (int i = 0; i < grid.size() - 1; i++) {
     for (int j = 0; j < grid[i].size() - 1; j++) {
       int last = center + current - 2;
@@ -49,10 +47,8 @@ int main() {
   for (int i = 1; i <= n; ++i) {
     int u;
     cin >> u;
-    
     cout << "Scenario #" << i << ":\n";
     int m = 2 * u + 1;
-    
     // initialize grid
     vvc grid(m);
     for (int k = 0; k < m; k++) {
@@ -62,23 +58,19 @@ int main() {
       }
       grid[k] = temp;
     }
-    
     vvvc slices;
     for (int j = 1; j <= m / 2 + 1; j++) {
       draw(grid, j);
       slices.push_back(grid);
     }
-    
     // print the first ones
     for (int j = 0; j < slices.size() - 1; j++) {
       cout << "slice #" << j + 1 << ":\n";
       print(slices[j], j);
     }
-    
     // print the central one
     cout << "slice #" << slices.size() << ":\n";
     print(slices[slices.size() - 1], u);
-    
     // print the last ones
     for (int j = slices.size() - 2; j >= 0; j--) {
       cout << "slice #" << 2 * slices.size() - j - 1 << ":\n";
