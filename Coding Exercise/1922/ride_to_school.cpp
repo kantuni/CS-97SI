91ee9d0939085397aa830fde07bef1fd04d7c2bb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-08 18:19:17 +0400

    Update ride_to_school.cpp

--- Coding Exercise/1922/ride_to_school.cpp
+++ Coding Exercise/1922/ride_to_school.cpp
@@ -7,28 +7,22 @@ int main() {
   while (true) {
     int n;
     cin >> n;
-    
     if (n == 0) {
       break;
     }
-    
     double tmin = DBL_MAX;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
       int vi, ti;
       cin >> vi >> ti;
-      
       if (ti < 0) {
         continue;
       }
-      
       double t = ti + (4.5 * 3600) / vi;
       if (t < tmin) {
         tmin = t;
       }
     }
-    
-    cout << ceil(tmin) << "\n";
+    cout << ceil(tmin) << endl;
   }
-  
   return 0;
 }
