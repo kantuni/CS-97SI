68e80362db2252aac3e54899a45dce85dc280c74
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-08 18:20:29 +0400

    Update yeehaa.cpp

--- Mathematics/1799/yeehaa.cpp
+++ Mathematics/1799/yeehaa.cpp
@@ -8,27 +8,22 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  
   for (int i = 1; i <= N; i++) {
     double R;
     int n;
     cin >> R >> n;
-    
     if (n == 2) {
-      cout << "Scenario #" << i << ":" << "\n";
+      cout << "Scenario #" << i << ":" << endl;
       cout << fixed << setprecision(3);
-      cout << R / 2.0 << "\n\n";
+      cout << R / 2.0 << endl << endl;
       continue;
     }
-    
     double a = 360.0 / n;
     double b = (180.0 - a) / 2;
-    
     double r = R / (2 * sin(b * PI / 180) / sin(a * PI / 180) + 1);
-    cout << "Scenario #" << i << ":" << "\n";
+    cout << "Scenario #" << i << ":" << endl;
     cout << fixed << setprecision(3);
-    cout << r << "\n\n";
+    cout << r << endl << endl;
   }
-  
   return 0;
 }
