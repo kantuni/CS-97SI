250cce4b84413daac94c6fa40055a3b431ae11c8
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-15 18:50:01 +0400

    Update expressions.cpp

--- Data Structures/3367/expressions.cpp
+++ Data Structures/3367/expressions.cpp
@@ -15,17 +15,13 @@ struct Node {
 int main() {
   int T;
   cin >> T;
-  
   for (int t = 0; t < T; t++) {
     string postfix;
     cin >> postfix;
-    
     map<char, Node> tree;
     stack<char> s;
-    
     for (int i = 0; i < postfix.size(); i++) {
       char letter = postfix[i];
-      
       if (islower(letter)) {
         s.push(letter);
       } else {
@@ -36,7 +32,6 @@ int main() {
           rchild = tree[s.top()];
         }
         s.pop();
-        
         Node lchild;
         if (tree.count(s.top()) == 0) {
           lchild.label = s.top();
@@ -44,7 +39,6 @@ int main() {
           lchild = tree[s.top()];
         }
         s.pop();
-        
         Node parent;
         parent.label = letter;
         parent.children = make_pair(lchild.label, rchild.label);
@@ -52,11 +46,9 @@ int main() {
         s.push(letter);
       }
     }
-    
     queue<char> q;
     char root = s.top();
     q.push(root);
-    
     string answer;
     while (!q.empty()) {
       answer += q.front();
@@ -68,10 +60,8 @@ int main() {
       }
       q.pop();
     }
-    
     reverse(answer.begin(), answer.end());
     cout << answer << "\n";
   }
-  
   return 0;
 }
