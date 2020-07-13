c81bab9ed5194235f959369afccbe3176f8aaa16
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-07-13 16:06:35 +0400

    Update nearest_common_ancestors.cpp

--- Data Structures/1330/nearest_common_ancestors.cpp
+++ Data Structures/1330/nearest_common_ancestors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   int T;
   cin >> T;
-  for (int t = 0; t < T; ++t) {
+  for (int t = 0; t < T; t++) {
     int n;
     cin >> n;
     vector<int> anc(n + 2, 0);
@@ -22,7 +22,7 @@ int main() {
       if (x > 0) {
         x = anc[x] > 0 ? anc[x] : -1;
         if (common[x] == 1) {
-          cout << x << "\n";
+          cout << x << endl;
           break;
         } else {
           common[x] = 1;
@@ -31,7 +31,7 @@ int main() {
       if (y > 0) {
         y = anc[y] > 0 ? anc[y] : -1;
         if (common[y] == 1) {
-          cout << y << "\n";
+          cout << y << endl;
           break;
         } else {
           common[y] = 1;
