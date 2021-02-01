51618dbfe5e41707fd606829040b034fb9450b91
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-02-01 16:52:13 +0400

    Update is-it-a-tree.cpp

--- Basic Graph Algorithms/1308/is-it-a-tree.cpp
+++ Basic Graph Algorithms/1308/is-it-a-tree.cpp
@@ -6,30 +6,26 @@ int main() {
   set<int> V, S;
   int E = 0, i = 1;
   bool tree = true;
-  
   while (true) {
     int s, t;
     cin >> s >> t;
-    
     if (s == 0 && t == 0) {
       bool c1 = E == 0;  // empty tree
       bool c2 = E == V.size() - 1;  // |E| = n - 1
       bool c3 = S.size() == V.size() - 1;  // has only one root
-      
       if (!c1 && !(tree && c2 && c3)) {
         tree = false;
       }
-      
       cout << "Case " << i << " is ";
-      if (!tree) cout << "not ";
+      if (!tree) {
+        cout << "not ";
+      }
       cout << "a tree." << "\n";
-      
       // start from scratch
       tree = true;
       E = 0;
       V.clear();
       S.clear();
-      
       // next case
       i++;
     } else if (s < 0 && t < 0) {
@@ -39,16 +35,13 @@ int main() {
     } else {
       V.insert(s);
       V.insert(t);
-      
       if (S.count(t) == 0) {
         S.insert(t);
       } else {
         tree = false;
       }
-      
       E++;
     }
   }
-  
   return 0;
 }
